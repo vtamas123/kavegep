48539ae4ce5f546e8e37f896dbed5269ad59a4de
vtamas123 <vargat12345@gmail.com>
2024-01-30 08:21:40 +0100

    egy

---
+++ kavegep/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kavegep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(  );
+            // Kápi
+            Console.WriteLine();
+            for (int i = 0; i < 15; i++)
+            {
+                
+            }
+        }
+    }
+}
